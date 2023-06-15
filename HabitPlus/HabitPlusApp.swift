50af11e0659f31f2a0e0593c854c3d70c41b304f
José Júnior <josejuninho8@gmail.com>
2023-06-15 14:53:49 -0300

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by José Júnior on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
